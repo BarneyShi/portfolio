5169c1c7a18904455fbb530efcd8816f4f1879d6
ZhuangzhuangShi <barneyshek@gmail.com>
2020-04-03 22:53:54 -0700

    Introduction finished

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+          <nav className='navbar bg-light'> 
+              <h2>
+                BARNEY SHI
+              </h2>
+          </nav>
+          <div className='jumbotron'>
+              <img className='rounded-circle' id='avator-picture' alt='avator' src='brian.jpg' />
+              <h2>Barney Shi</h2>
+          </div>
+          <div className='container'>
+              <div className='row'>
+                  <div className='col-4  text-left' id='introduction'>
+                      <p>About me</p>
+                      <p>I'm a second-degree compute science student at University of British Columbia. I graduated with a Bachelor degree of Economics in 2018.
+                         I'm interested in web development.  
+                      </p>
+                  </div>
+                  <div className='col-4 offset-4'>
+                  <p>Project</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+    )
+  }
 }
 
 export default App;
